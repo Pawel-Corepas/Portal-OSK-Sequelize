a816c7c964f5c33350527e9c941b7e3fef27e1cf
skorniewski.pawel@gmail.com <skorniewski.pawel@gmail.com>
2021-02-22 22:02:29 +0100

    api update

--- api/utils/resourceIdDecoder.js
+++ api/utils/resourceIdDecoder.js
@@ -1,4 +1,17 @@
 function resourceIdDecoder(id) {
+    var crypto = require('crypto'),
+    algorithm = 'aes-256-ctr',
+    secret = 'd6F3Efeq';
+    let key = crypto.createHash('sha256').update(String(secret)).digest('base64').substr(0, 32);
+    return decrypt(id);
+
+function decrypt(text){
+    var decipher = crypto.createDecipheriv('aes-256-cbc',
+    key, 'elcarvector12341')
+    var dec = decipher.update(text, 'hex', 'utf8')
+    dec += decipher.final('utf8')
+    return dec
+  }
 
 }
 
--- api/utils/resourceIdEncoder.js
+++ api/utils/resourceIdEncoder.js
@@ -14,18 +14,6 @@ function resourceIdEncoder(id) {
         crypted += cipher.final('hex')
         return crypted
     }
-
-    function decrypt(text){
-        var decipher = crypto.createDecipheriv('aes-256-cbc',
-        key, 'elcarvector12341')
-        var dec = decipher.update(text, 'hex', 'utf8')
-        dec += decipher.final('utf8')
-        return dec
-      }
-
-    var hw = encrypt(id)
-    // outputs hello world
-    console.log(decrypt(hw));
 }
 
 module.exports = resourceIdEncoder
